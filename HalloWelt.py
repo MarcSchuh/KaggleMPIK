df5f734818395a982420f6116871efe6c04b51c7
Thomas Hugle <thugle@mpi-hd.mpg.de>
2020-07-30 22:55:50 +0200

    test 2

--- HalloWelt.py
+++ HalloWelt.py
@@ -2,6 +2,6 @@ from kaggle_environments import evaluate, make
 env = make("halite", configuration={"episodeSteps": 400}, debug=True)
 print(env.configuration)
 
-print('test')
+print('test2')
 print('branch')
-print('test')
+print('test2')
