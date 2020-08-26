f7782fa5d075b7b62a6c4792cc917e1a306e0191
vale <v.tenorth@gmx.de>
2020-08-26 21:05:37 +0200

    avoid merge conflicts

--- submission.py
+++ submission.py
@@ -1,20 +1,41 @@
 from kaggle_environments.envs.halite.helpers import *
 from random import choice
 
-# Score ########################################################################################################
+# Variables
+###################################################################################################
+max_ships = 20
+max_shipyards = 5
 
 
-# Scheduler ####################################################################################################
+# Score ###################################################################################################
+
+
+# Scheduler ###################################################################################################
+
+#Priosierung der Schiffe
+def shipyard_plan(turn, halite, ships): # decide if a shipyard spawns a new ship
+    if halite > 500 & ships <= max_ships:
+        return "SPAWN"
+    else:
+        return "NONE"
 
 
 # Pathfinder ###################################################################################################
 
 
-# Agent ########################################################################################################
+# Agent ###################################################################################################
 
 def agent(obs, config):
     board = Board(obs, config)
     me = board.current_player
+    player = obs.player
+    size = config.size
+    board_halite = obs.halite
+    board = Board(obs, config)
+    current_halite, shipyards, ship_items = obs.players[player]
+    shipyard_uids = list(shipyards.keys())
+    shipyards = list(shipyards.values())
+    ship_number = len(ship_items)
 
     # Set actions for each ship
     for ship in me.ships:
@@ -22,6 +43,7 @@ def agent(obs, config):
 
     # Set actions for each shipyard
     for shipyard in me.shipyards:
-        shipyard.next_action = None
+        shipyard.next_action = shipyard_plan(turn, current_halite, ship_number)
 
     return me.next_actions
+
