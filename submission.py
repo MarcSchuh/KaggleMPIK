df152e65e41ee821c20435bc8e2c1b19dd11ec8c
Thomas Hugle <thugle@mpi-hd.mpg.de>
2020-08-31 01:00:24 +0200

    Added opening strategy and score function, plus small corrections to old code

--- submission.py
+++ submission.py
@@ -1,40 +1,39 @@
 from kaggle_environments.envs.halite.helpers import *
-from random import choice
 import numpy as np
 
 
 # Boards #######################################################################################################
 
-def board_halite(obs: Dict[str, Any], config: Dict[str, Any]) -> np.ndarray:
+def board_halite_(obs: Dict[str, Any], config: Dict[str, Any]) -> np.ndarray:
     return np.array(obs['halite']).reshape(config['size'], -1)
 
 
-def board_ships(obs: Dict[str, Any], config: Dict[str, Any]) -> np.ndarray:
-    board_ships_ = np.empty((config['size'], config['size']))
-    board_ships_[:] = np.nan
+def board_ships_(obs: Dict[str, Any], config: Dict[str, Any]) -> np.ndarray:
+    board_ships = np.empty((config['size'], config['size']))
+    board_ships[:] = np.nan
 
     for player in range(len(obs['players'])):
         ships_dict = obs['players'][player][2]
         for ship in ships_dict:
             x_pos = ships_dict[ship][0] % config['size']
             y_pos = ships_dict[ship][0] // config['size']
-            board_ships_[x_pos, y_pos] = player
+            board_ships[x_pos, y_pos] = player
 
-    return board_ships_
+    return board_ships
 
 
-def board_shipyards(obs: Dict[str, Any], config: Dict[str, Any]) -> np.ndarray:
-    board_shipyards_ = np.empty((config['size'], config['size']))
-    board_shipyards_[:] = np.nan
+def board_shipyards_(obs: Dict[str, Any], config: Dict[str, Any]) -> np.ndarray:
+    board_shipyards = np.empty((config['size'], config['size']))
+    board_shipyards[:] = np.nan
 
     for player in range(len(obs['players'])):
         shipyards_dict = obs['players'][player][1]
         for shipyard in shipyards_dict:
-            x_pos = shipyards_dict[shipyard][0] % config['size']
-            y_pos = shipyards_dict[shipyard][0] // config['size']
-            board_shipyards_[x_pos, y_pos] = player
+            x_pos = shipyards_dict[shipyard] % config['size']
+            y_pos = shipyards_dict[shipyard] // config['size']
+            board_shipyards[x_pos, y_pos] = player
 
-    return board_shipyards_
+    return board_shipyards
 
 
 # Distance #####################################################################################################
@@ -81,8 +80,9 @@ def create_distance_matrix(size: int) -> np.ndarray:
 
 # Score ########################################################################################################
 
-def score(board_halite_: np.ndarray) -> np.ndarray:
-    return board_halite_
+def score(board_halite: np.ndarray, ship_position: int, size: int, distance_matrix: np.ndarray) -> np.ndarray:
+    # Halite per turn with ship starting from shipyard
+    return board_halite / (1 + 2 * distance_matrix[ship_position].reshape(size, -1))
 
 
 # Scheduler ####################################################################################################
@@ -93,22 +93,54 @@ def scheduler():
 
 # Pathfinder ###################################################################################################
 
-def pathfinder():
-    pass
+def pathfinder(current_position: int, target_position: int, size: int) -> str:
+    current_coordinates = get_coordinates(current_position, size)
+    target_coordinates = get_coordinates(target_position, size)
+
+    return 'NORTH'
 
 
 # Agent ########################################################################################################
 
 def agent(obs: Dict[str, Any], config: Dict[str, Any]) -> Dict[str, str]:
-    board = Board(obs, config)
-    me = board.current_player
+    player_id = obs['player']
+    actions = {}
 
-    # Set actions for each ship
-    for ship in me.ships:
-        ship.next_action = choice([ShipAction.NORTH, ShipAction.EAST, ShipAction.SOUTH, ShipAction.WEST, None])
+    distance_matrix = create_distance_matrix(config['size'])
 
-    # Set actions for each shipyard
-    for shipyard in me.shipyards:
-        shipyard.next_action = None
+    board_halite = board_halite_(obs, config)
+    board_ships = board_ships_(obs, config)
+    board_shipyards = board_shipyards_(obs, config)
 
-    return me.next_actions
+    shipyards_dict = obs['players'][player_id][1]
+    ships_dict = obs['players'][player_id][2]
+
+    if obs['step'] == 0:
+        # Build a shipyard in the first turn
+        for ship in ships_dict:
+            actions[ship] = 'CONVERT'
+
+    # Spawn ships in the first 10 rounds
+    elif obs['step'] <= 10:
+        for shipyard in shipyards_dict:
+            actions[shipyard] = 'SPAWN'
+
+        for ship in ships_dict:
+            ship_position = ships_dict[ship][0]
+            ship_score = score(board_halite, ship_position, config['size'], distance_matrix)
+            target_coordinates = np.array(np.unravel_index(np.argmax(ship_score), ship_score.shape))
+            target_position = get_position(target_coordinates, config['size'])
+            actions[ship] = pathfinder(ship_position, target_position, config['size'])
+
+    else:
+        # for shipyard in shipyards_dict:
+        #     actions[shipyard] = None
+
+        for ship in ships_dict:
+            ship_position = ships_dict[ship][0]
+            ship_score = score(board_halite, ship_position, config['size'], distance_matrix)
+            target_coordinates = np.array(np.unravel_index(np.argmax(ship_score), ship_score.shape))
+            target_position = get_position(target_coordinates, config['size'])
+            actions[ship] = pathfinder(ship_position, target_position, config['size'])
+
+    return actions
